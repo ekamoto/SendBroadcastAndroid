262672a604b3db880f655da1bd3904d6c5172b1d
Leandro Shindi Ekamoto <leandro@localhost.localdomain>
2015-01-09 10:36:11 -0300

    Subindo Projeto

---
+++ src/com/example/SendBroadcast/MyActivity.java
@@ -0,0 +1,34 @@
+package com.example.SendBroadcast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MyActivity extends Activity {
+
+    private Button btnEnviar;
+    public Intent intent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        String teste = "HisamotoBroadCast";
+        intent = new Intent(teste);
+
+        btnEnviar = (Button)findViewById(R.id.sendBroadcast);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("nome", "Leandro Shindi Ekamoto vindo por broadcast");
+                getApplication().sendBroadcast(intent);
+            }
+        });
+    }
+}
